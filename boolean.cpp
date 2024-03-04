9ad32d6e4ca6f0eeb15cd822f29b22155949627f
Jainrani <ranijain.cs@gmail.com>
2024-03-04 14:12:01 +0530

    first commit

---
+++ boolean.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool isCodingFun=true;
+    bool isNotGood=false;
+    cout<<isCodingFun<<"\n";
+    cout<<isNotGood<<"\n";
+    return 0;
+}
---
+++ int,float,double.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int myNum = 1000;
+float num = 2.75;
+double myNumm = 6.54;
+cout << myNum <<"\n";
+cout<< num <<"\n";
+cout<< myNumm;
+return 0;
+}
